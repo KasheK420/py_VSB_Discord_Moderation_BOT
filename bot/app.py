0969f5ef3e818972ba2ca90fc7361b41ab09cf81
Jakub Vitásek <quvixcz@gmail.com>
2025-06-13 22:19:07 +0200

    fix app.py format error

--- bot/app.py
+++ bot/app.py
@@ -4,7 +4,7 @@ import argparse
 def main(args):
     # Rozhodně by se to nemělo startovat přes tenhle import, ale nemám nervy ted přepisovat ten startup :D
     # (future proofing) Až se to přepíše a udělá se z toho funkce/třída tak tam passnout případné spouštěcí argumenty
-    import vsb_bot.startup   # noqa: F401, I001
+    import vsb_bot.startup
 
 
 if __name__ == "__main__":
